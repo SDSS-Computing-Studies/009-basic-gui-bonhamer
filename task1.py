c6c3ecba845c5682f967415ae46315b3648cbb2c
bonhamer <90730645+bonhamer@users.noreply.github.com>
2021-12-13 13:48:30 -0800

    1

---
+++ task1.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import  *
+
+window = tk.Tk()
+entry1 = tk.Entry(window, borderwidth=3, relief=SUNKEN)
+entry1.grid(row=3, column = 1, columnspan=2)
+label1 = tk.Label(window,text="x")
+label1.grid(row=3, column = 6, columnspan=2)
+entry2 = tk.Entry(window, borderwidth=3, relief=SUNKEN)
+entry2.grid(row=3, column = 10, columnspan=2)
+label2 = tk.Label(window,text="=")
+label2.grid(row=3, column = 15, columnspan=2)
+entry3 = tk.Entry(window, borderwidth=3, relief=SUNKEN)
+entry3.grid(row=3, column = 20, columnspan=2)
+
+
+
+window.mainloop()
